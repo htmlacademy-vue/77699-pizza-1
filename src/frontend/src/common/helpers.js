d9ed1343d433f064d8925f47d798ddc1a3a0f554
Каракетова Дарья <d.karaketova@ctm.ru>
2021-07-09 09:56:32 +0300

    Корректировка синтаксиса

--- src/frontend/src/common/helpers.js
+++
@@ -1,8 +0,0 @@
-// Добавляем к объекту аттрибут type
-// из сопоставления { name: type }
-export const pizzaTypesMixin = (obj, types) => {
-  return (obj || {}).map((item) => ({
-    ...item,
-    type: types[item.name],
-  }));
-};
