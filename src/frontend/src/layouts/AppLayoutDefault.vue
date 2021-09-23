1df915f46b91e58a5501a18ef3ef3ae992ada057
Каракетова Дарья <d.karaketova@ctm.ru>
2021-09-23 11:18:49 +0300

    3.14. Маршрутизатор и лейауты

---
+++ src/frontend/src/layouts/AppLayoutDefault.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <header class="header">
+      <div class="header__logo">
+        <router-link :to="{ name: 'IndexHome' }" class="logo">
+          <img
+            src="~@/assets/img/logo.svg"
+            alt="V!U!E! Pizza logo"
+            width="90"
+            height="40"
+          />
+        </router-link>
+      </div>
+      <div class="header__cart">
+        <router-link :to="{ name: 'Cart' }">0 ₽</router-link>
+      </div>
+      <div class="header__user">
+        <router-link :to="{ name: 'Login' }" class="header__login"
+          ><span>Войти</span></router-link
+        >
+      </div>
+    </header>
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppLayoutDefault",
+};
+</script>
+
+<style lang="scss" scoped></style>
